8dc17bc0a44bceddda0ada5fbe402bf2ccbe0869
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 15:06:31 +0100

    Create Bike.java

---
+++ src/Bike.java
@@ -0,0 +1,18 @@
+package tutorial;
+
+public class Bike extends Vehicle {
+	
+	public Bike() {
+		this("", "");
+	}
+	
+	public Bike(String brand, String make) {
+		super(brand, make);
+		passenger = 1;
+	}
+	
+	public void drive() {
+		System.out.println("Ring Ring!");
+	}
+
+}
